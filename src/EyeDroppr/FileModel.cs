8ff2d84e94f77f6fc8d2b0ee1dfcf55d0a9d1161
Christopher Bauer <Chrisbauer.career@gmail.com>
2018-04-29 01:03:58 -0400

    Fix types

--- src/EyeDroppr/FileModel.cs
+++ src/EyeDroppr/FileModel.cs
@@ -8,7 +8,6 @@ namespace EyeDroppr
     public class FileModel
     {
         public string FileName { get; set; }
-        public BitmapImage FileImage { get; set; }
-        public Task<BitmapImage> ImageSourceAsync { get; set; }
+        public ImageSource FileImage { get; set; }
     }
 }
